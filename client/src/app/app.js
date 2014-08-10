28f991708007c9cfb78e74ad364acae0d42089bf
Dirk Makowski <dmakowski@gmx.net>
2014-08-10 22:01:30 +0200

    Interceptor for HTTP errors

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,6 +1,33 @@
-define(PYM_APP_REQUIREMENTS, function (angular) {
+define(PYM_APP_REQUIREMENTS,
+           // ng,      pym/pym
+    function (angular, PYM) {
     'use strict';
 
     var PymApp = angular.module('PymApp', PYM_APP_INJECTS);
+
+
+    PymApp.config(
+      [
+                '$httpProvider', '$provide',
+        function($httpProvider,   $provide) {
+            $provide.factory('PymHttpErrorInterceptor',
+              [
+                        '$q',
+                function($q) {
+                    return {
+                        responseError: function(rejection) {
+                            PYM.growl({'kind': 'error', 'title': rejection.status, 'text': rejection.statusText});
+                            return $q.reject(rejection);
+                        }
+                    };
+                }
+              ]
+            );
+          $httpProvider.interceptors.push('PymHttpErrorInterceptor');
+        }
+      ]
+    );
+
+
     return PymApp;
 });
